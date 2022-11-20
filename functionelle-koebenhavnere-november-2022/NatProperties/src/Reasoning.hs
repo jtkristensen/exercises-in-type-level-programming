df9ab3b84422e61521d05781ee05e214798b2123
FurryRogue <joachim.tilsted@gmail.com>
2022-11-20 18:43:55 +0100

    better Reasoning library

--- functionelle-koebenhavnere-november-2022/NatProperties/src/Reasoning.hs
+++ functionelle-koebenhavnere-november-2022/NatProperties/src/Reasoning.hs
@@ -1,8 +1,34 @@
 {-# LANGUAGE PolyKinds
            , TypeFamilies
+           , TypeOperators
+           , GADTs
+           , RankNTypes
   #-}
-
 
 module Reasoning where
 
-data family Singleton ( t :: k)
+import Data.Kind (Type)
+
+data family Singleton ( t :: k )
+
+infix 3 :==:
+
+data a :==: b :: Type where
+  Reflexive :: a :==: a
+
+
+eqSymmetric :: a :==: b -> b :==: a
+eqSymmetric Reflexive = Reflexive
+
+eqCongruent :: (Singleton a -> Singleton b) -> (a :==: b) -> (f a :==: f b)
+eqCongruent _ Reflexive = Reflexive
+
+eqTransitive :: (a :==: b) -> (b :==: c) -> (a :==: c)
+eqTransitive Reflexive Reflexive = Reflexive
+
+infixr 1 %|
+(%|) :: (a :==: b) -> ((a ~ b) => r) -> r
+(%|) Reflexive x = x
+
+qed :: a :==: a
+qed = Reflexive

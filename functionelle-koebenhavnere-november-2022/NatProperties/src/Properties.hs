468b4a60729d9369a9d704aaea5975be490859ba
FurryRogue <joachim.tilsted@gmail.com>
2022-11-21 15:27:59 +0100

    don't give away the answers.

--- functionelle-koebenhavnere-november-2022/NatProperties/src/Properties.hs
+++ functionelle-koebenhavnere-november-2022/NatProperties/src/Properties.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TypeOperators
            , DataKinds
            , GADTs
-           , NoImplicitPrelude
   #-}
 
 module Properties where
@@ -9,40 +8,33 @@ module Properties where
 import Nat
 import Equality
 
+
 plus0
   :: SNat n
   -> n :+ 'Zero :==: n
 
-plus0  O    = reflexive
-plus0 (S n) = congruent (plus0 n)
+plus0 = undefined
 
 plusS
   :: SNat m -> SNat n
   -> m :+ 'Suc n :==: 'Suc m :+ n
 
-plusS  O    _ = reflexive
-plusS (S m) n =
-  congruent (plusS m n)
+plusS = undefined
 
 plusCommutative
   :: SNat m -> SNat n
   -> m :+ n :==: n :+ m
 
-plusCommutative  O    n = symmetric (plus0 n)
-plusCommutative (S m) n =
-     plusCommutative n m
-  %| symmetric (plusS n m)
+plusCommutative = undefined
 
 plusInjective
   :: SNat m -> SNat n -> SNat k
   -> m :==: n -> m :+ k :==: n :+ k
 
-plusInjective _ _ _ proof = proof %| qed
+plusInjective = undefined
 
 plusAssociative
   :: SNat m -> SNat n -> SNat k
   -> m :+ (n :+ k) :==: (m :+ n) :+ k
 
-plusAssociative  O    _ _ = qed
-plusAssociative (S m) n k =
-  congruent (plusAssociative m n k)
+plusAssociative  = undefined

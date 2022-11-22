cbd22edd6b09313269fa8af58a96dfd817ce4f71
FurryRogue <joachim.tilsted@gmail.com>
2022-11-22 11:51:57 +0100

    better multiplication exercise

--- functionelle-koebenhavnere-november-2022/NatProperties/src/Multiplication.hs
+++ functionelle-koebenhavnere-november-2022/NatProperties/src/Multiplication.hs
@@ -7,14 +7,13 @@ module Multiplication where
 
 import Nat
 import Equality
-import qualified Addition
+-- import qualified Addition
 
 zero
   :: SNat n
   -> n :* 'Zero :==: 'Zero
 
-zero  O    = qed
-zero (S n) = zero n
+zero = undefined
 
 -- no suc rule
 
@@ -22,26 +21,22 @@ commutative
   :: SNat m -> SNat n
   -> m :* n :==: n :* m
 
-commutative  O    n = symmetric (zero n)
-commutative (S m) O = commutative m O
-commutative (S m) (S n) =
-  congruent $
-     commutative m (S n)
-  %| commutative n (S m)
-  %| commutative n    m
-  %| Addition.associative m n (m %* n)
-  %| Addition.associative n m (m %* n)
-  %| Addition.commutative m n
-  %| qed
+commutative = undefined
 
 injective
   :: SNat m -> SNat n -> SNat k
   -> m :==: n -> m :* k :==: n :* k
 
-injective = undefined
+injective _ _ _ Reflexive = qed
+
+distributive
+  :: SNat k -> SNat m -> SNat n
+  -> k :* (m :+ n) :==: (k :* n) :+ (k :* m)
+
+distributive = undefined
 
 associative
   :: SNat m -> SNat n -> SNat k
   -> m :* (n :* k) :==: (m :* n) :* k
 
-associative = undefined
+associative  = undefined

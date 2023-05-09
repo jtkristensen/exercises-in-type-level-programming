83377027adb2a3313a592c4874551c1a5beb2377
Joachim <joachim.tilsted@gmail.com>
2023-05-09 16:02:12 +0200

    first case for multiplication

--- functionelle-koebenhavnere-november-2022/NatProperties/src/Multiplication.hs
+++ functionelle-koebenhavnere-november-2022/NatProperties/src/Multiplication.hs
@@ -7,13 +7,14 @@ module Multiplication where
 
 import Nat
 import Equality
--- import qualified Addition
+import qualified Addition
 
 zero
   :: SNat n
   -> n :* 'Zero :==: 'Zero
 
-zero = undefined
+zero  O    = Reflexive
+zero (S n) = zero n
 
 -- no suc rule
 
@@ -21,7 +22,16 @@ commutative
   :: SNat m -> SNat n
   -> m :* n :==: n :* m
 
-commutative = undefined
+commutative  O     n    = symmetric (zero n)
+commutative (S m)  O    = zero m
+commutative (S m) (S n) =
+     commutative m (S n)
+  %| commutative n (S m)
+  %| commutative n m
+  %| Addition.associative n m (m %* n)
+  %| Addition.associative m n (m %* n)
+  %| Addition.commutative n m
+  %| qed
 
 injective
   :: SNat m -> SNat n -> SNat k
